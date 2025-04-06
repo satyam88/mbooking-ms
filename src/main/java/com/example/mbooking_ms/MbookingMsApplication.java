029b3f5735f8f207ae634002a2713f064be6170a
cloudanddevops88 <satyampandey88@gmail.com>
2025-04-06 10:25:59 +0530

    coderefactor

---
+++ src/main/java/com/example/mbooking_ms/MbookingMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mbooking_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbookingMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbookingMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mbooking_ms/MbookingMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mbooking_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbookingMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
